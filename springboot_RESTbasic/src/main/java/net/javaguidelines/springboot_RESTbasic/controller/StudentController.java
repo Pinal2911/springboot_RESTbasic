816c1e063f111b8f28d8c4cfe38589eac78e48bb
Pinal2911 <pinalparmar270@gmail.com>
2024-01-27 12:30:56 +0530

    base controller added

--- springboot_RESTbasic/src/main/java/net/javaguidelines/springboot_RESTbasic/controller/StudentController.java
+++ springboot_RESTbasic/src/main/java/net/javaguidelines/springboot_RESTbasic/controller/StudentController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//note -> use postman to GET,PUT,DELETE requests
+//base url-> http://localhost:8080/student
 @RestController
+//base controller
+@RequestMapping("students")
 public class StudentController {
     //response_entity_example
+    //what is response entity:
     //ResponseEntity represents the whole HTTP response : status code,headers, and body
     //as a result we can use it to fully configure the HTTP response
     //if we want to use it, we have to return it from the endpoint; spring takes care of the rest
@@ -24,28 +28,31 @@ public class StudentController {
                 "Pinal",
                 "Parmar"
         );
-        return new ResponseEntity<>(s,HttpStatus.OK);
+        //send general response -> return new ResponseEntity<>(s,HttpStatus.OK);
         //or second way to return response
         //below does the same as above
         //return ResponseEntity.ok(s);
+
+        //add custom header as well
+        return ResponseEntity.ok().header("custom-header","pinal").body(s);
     }
 
     //following is the way to return List as a json using REST API
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents(){
         List<Student> students=new ArrayList<>();
         students.add(new Student(1,"Pinal","Parmar"));
         students.add(new Student(2,"Tom","Dsouza"));
         students.add(new Student(3,"ABC","PQR"));
 
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     //SPRING BOOT REST API with path variable
     //http://localhost:8080/students/1 - handling single path variable
 
-    @GetMapping("/students/{id}")
+    @GetMapping("{id}")
     public Student studentPathVar(@PathVariable int id){
         return new Student(id,"Pin","Parmar");
     }
@@ -53,7 +60,7 @@ public class StudentController {
     //handling multiple path variables with different
     //name in URI and in function
     //http://localhost:8080/students/1/tom/jery
-    @GetMapping("/students/{id}/{first-name}/{last-name}")
+    @GetMapping("{id}/{first-name}/{last-name}")
     public Student studentPathMultiple(@PathVariable("id") int sid,
                                        @PathVariable("first-name") String fname,
                                        @PathVariable("last-name") String lname){
@@ -62,7 +69,7 @@ public class StudentController {
 
     //request param
     //http://localhost:8080/students/query?id=1
-    @GetMapping("students/query")
+    @GetMapping("query")
     public Student studentRequestVariable(@RequestParam int id){
         return new Student(id,"pinalll","parmarr");
     }
@@ -71,7 +78,7 @@ public class StudentController {
     //@RequestBody- is responsible for retrieving the HTTP request body and automatically
     //converting it to the java object
     //@ResponseStatus is used for indicating status of request (200,404,etc)
-    @PostMapping("/students/create")
+    @PostMapping("create")
     @ResponseStatus(HttpStatus.CREATED)
     public Student createStudent(@RequestBody Student student){
         System.out.println(student.getId());
@@ -85,7 +92,7 @@ public class StudentController {
     //Springboot REST API that handles HTTP PUT Request-updating existing resource
     //putmapping-usd for mapping HTTP PUT request onto specific handler method
 
-    @PutMapping("students/{id}/update")
+    @PutMapping("{id}/update")
     public Student updateStudent(@RequestBody Student student,@PathVariable("id") int studentId){
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
@@ -95,7 +102,7 @@ public class StudentController {
 
 
     //spring boot REST API that handles HTTP DELETE Request - deleting the existing resource
-    @DeleteMapping("students/{id}/delete")
+    @DeleteMapping("{id}/delete")
     public String deleteStudent(@PathVariable("id") int studentId){
         System.out.println(studentId);
         return "student deleted successfully";
